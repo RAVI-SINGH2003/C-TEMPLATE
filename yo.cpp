116985675fbdac80bd33e2ad0d571b8f8a8b4eea
RAVI-SINGH <ravirainbow2003@gmail.com>
2023-02-25 19:55:34 +0530

    first-commit

---
+++ yo.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+void init_code()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+void solve()
+{
+   cout << 1;
+}
+int main()
+{
+    init_code();
+     int tt;
+    cin >> tt;
+    while (tt--)
+        solve();
+}
